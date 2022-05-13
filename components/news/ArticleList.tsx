822ae2a9bf6a61e7e2bb428502669e57e8b0e735
Ludvig Svedberg <ludvig.svedberg@mac.com>
2022-05-13 15:05:03 +0200

    Fix "infinte scroll" for news

--- components/news/ArticleList.tsx
+++ components/news/ArticleList.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import { StyleSheet } from 'react-native';
 import { FlatList, Text } from '../../components/Themed';
-import { useNewsPageQuery } from '../../generated/graphql';
+import { NewsPageQuery, useNewsPageQuery } from '../../generated/graphql';
 import Article from './Article';
 
 const ArticleList = () => {
+  const [amountToLoad, setAmountToLoad] = React.useState(10);
+  const [loadedPages, setLoadedPages] = React.useState(1);
   const { loading, data, fetchMore, refetch } = useNewsPageQuery({
-    variables: { page_number: 0, per_page: 10 },
+    variables: { page_number: 0, per_page: amountToLoad },
   });
   const articles = data?.news?.articles;
 
@@ -21,8 +23,15 @@ const ArticleList = () => {
       data={articles}
       keyExtractor={(article) => article.id}
       refreshing={false}
-      onRefresh={refetch}
-      onEndReached={() => fetchMore({ variables: { page_number: articles.length } })}
+      onRefresh={() => refetch()}
+      onEndReached={() => {
+        console.log('end');
+        if (data.news.pageInfo.totalPages > loadedPages) {
+          setAmountToLoad((current) => current + 10);
+          setLoadedPages((current) => current + 1);
+        }
+      }}
+      onEndReachedThreshold={0.5}
     />
   );
 };
