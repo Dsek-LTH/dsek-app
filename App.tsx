100444784713429ae123c1fc07c255093f8601c8
Ludvig <ludvig.svedberg@mac.com>
2023-02-21 19:02:56 +0100

    Remove old code and libs

--- App.tsx
+++ App.tsx
@@ -1,10 +1,6 @@
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import MainView from '~/components/MainView';
 import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import { ApiAccessProvider } from './providers/ApiAccessProvider';
-import LoginProvider from './providers/LoginProvider';
-import { UserProvider } from './providers/UserProvider';
 
 const App = () => {
   const isLoadingComplete = useCachedResources();
