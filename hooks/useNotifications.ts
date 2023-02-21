0b08758e0894bb68217521f25a946762c372156a
Ludvig <ludvig.svedberg@mac.com>
2023-02-21 18:49:57 +0100

    Add correct channel ids for Android

--- hooks/useNotifications.ts
+++ hooks/useNotifications.ts
@@ -13,6 +13,52 @@ Notifications.setNotificationHandler({
 
 const registerForPushNotificationsAsync = async () => {
   let token;
+
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('LIKE', {
+      name: 'Likes',
+      description: 'When someone likes your posts',
+      importance: Notifications.AndroidImportance.LOW,
+    });
+    Notifications.setNotificationChannelAsync('COMMENT', {
+      name: 'Comments',
+      description: 'When someone comments on your posts',
+      importance: Notifications.AndroidImportance.DEFAULT,
+    });
+    Notifications.setNotificationChannelAsync('MENTION', {
+      name: 'Mentions',
+      description: 'When someone mentions you',
+      importance: Notifications.AndroidImportance.DEFAULT,
+    });
+    Notifications.setNotificationChannelAsync('NEW_ARTICLE', {
+      name: 'News relevant to you',
+      description: 'When there is a new article with a tag you are subscribed to',
+      importance: Notifications.AndroidImportance.HIGH,
+    });
+    Notifications.setNotificationChannelAsync('EVENT_LIKE', {
+      name: 'Likes on your events',
+      importance: Notifications.AndroidImportance.LOW,
+    });
+    Notifications.setNotificationChannelAsync('EVENT_GOING', {
+      name: "Going to your event",
+      description: 'When someone wants to go to your event',
+      importance: Notifications.AndroidImportance.LOW,
+    });
+    Notifications.setNotificationChannelAsync('EVENT_INTERESTED', {
+      name: "Interested in your event",
+      description: 'When someone is interested in your event',
+      importance: Notifications.AndroidImportance.LOW,
+    });
+    Notifications.setNotificationChannelAsync('CREATE_MANDATE', {
+      name: "New position",
+      description: 'When you get a new volunteer position (funktionärspost)',
+      importance: Notifications.AndroidImportance.DEFAULT,
+    });
+    Notifications.setNotificationChannelAsync('IMPORTANT', {
+      name: 'Important notifications',
+      description: 'Notifications that are REALLY important, should be very rare',
+      importance: Notifications.AndroidImportance.MAX,
+    });
   if (Device.isDevice) {
     const { status: existingStatus } = await Notifications.getPermissionsAsync();
     let finalStatus = existingStatus;
@@ -27,13 +73,7 @@ const registerForPushNotificationsAsync = async () => {
   } else {
     alert('Must use physical device for Push Notifications');
   }
-
-  if (Platform.OS === 'android') {
-    Notifications.setNotificationChannelAsync('default', {
-      name: 'default',
-      importance: Notifications.AndroidImportance.HIGH,
-      // lightColor: '#FF231F7C',
-    });
+    
   }
   return token;
 };
