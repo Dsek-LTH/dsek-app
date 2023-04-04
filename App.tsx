7436efeab8309ea7514eb1da99450189e1a3b332
Ludvig <ludvig.svedberg@mac.com>
2023-04-04 23:41:55 +0200

    Remove useCachedResources

--- App.tsx
+++ App.tsx
@@ -1,20 +1,15 @@
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import MainView from '~/components/MainView';
-import useCachedResources from './hooks/useCachedResources';
 import * as SplashScreen from 'expo-splash-screen';
+
 SplashScreen.preventAutoHideAsync();
 const App = () => {
-  const isLoadingComplete = useCachedResources();
 
-  if (!isLoadingComplete) {
-    return null;
-  } else {
     return (
       <SafeAreaProvider>
         <MainView />
       </SafeAreaProvider>
     );
-  }
 };
 
 export default App;
