38d7ae3e21ad42dcb866bc0a2c30dec906b1d2f3
Ludvig <ludvig.svedberg@mac.com>
2023-04-05 15:03:53 +0200

    Add support for local dark/light mode which responds to site

--- App.tsx
+++ App.tsx
@@ -1,15 +1,22 @@
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import MainView from '~/components/MainView';
 import * as SplashScreen from 'expo-splash-screen';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { useColorScheme } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+import MainView from '~/components/MainView';
 
 SplashScreen.preventAutoHideAsync();
 const App = () => {
+  const preferredColorScheme = useColorScheme();
+  const [colorScheme, setColorScheme] = React.useState<'light' | 'dark'>(preferredColorScheme);
 
-    return (
-      <SafeAreaProvider>
-        <MainView />
-      </SafeAreaProvider>
-    );
+  return (
+    <SafeAreaProvider>
+      <MainView colorScheme={colorScheme} onColorChange={setColorScheme} />
+      <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
+    </SafeAreaProvider>
+  );
 };
 
 export default App;
--- components/MainView.tsx
+++ components/MainView.tsx
@@ -1,14 +1,50 @@
-import React, { useEffect, useRef } from 'react';
-import { BackHandler, Platform, SafeAreaView, Linking, View, Text, ActivityIndicator, ScrollView, RefreshControl } from 'react-native';
-import WebView from 'react-native-webview';
-import NotificationProvider from '~/providers/NotificationProvider';
 import * as SplashScreen from 'expo-splash-screen';
+import React, { useEffect, useRef } from 'react';
+import {
+  ActivityIndicator,
+  BackHandler,
+  Linking,
+  Platform,
+  SafeAreaView,
+  Text,
+  View,
+} from 'react-native';
+import WebView from 'react-native-webview';
 
 const WEBSITE_URL = 'https://app.dsek.se';
 
-const MainView: React.FC = () => {
+const DARK_COLOR = '#121212';
+const LIGHT_COLOR = '#fff';
+
+const INTIIAL_JAVASCRIPT_CODE = `(function() {
+  const mode = window.localStorage.getItem('mode');
+  const updateMode = (newMode) => {
+    if (newMode) {
+      window.ReactNativeWebView.postMessage(newMode);
+    }
+  }
+  updateMode(mode);
+
+  const method = "setItem";
+  const object = window.localStorage;
+  const original = object[method].bind(object);
+  const newMethod = function (...args) {
+    if (Array.isArray(args) && args[0] === 'mode') {
+      updateMode(args[1]);
+    }
+    const result = original.apply(null, args);
+    return result;
+  };
+  object[method] = newMethod.bind(object);
+})();`;
+
+const MainView: React.FC<{
+  colorScheme: 'light' | 'dark';
+  onColorChange: (mode: 'dark' | 'light') => void;
+}> = ({ colorScheme, onColorChange }) => {
   const webViewRef = useRef<WebView>(null);
   const [isLoading, setIsLoading] = React.useState(true);
+  const backgroundColor = colorScheme === 'light' ? LIGHT_COLOR : DARK_COLOR;
 
   /* for swipe navigation (and back button) on Android */
   const onAndroidBackPress = () => {
@@ -32,26 +68,34 @@ const MainView: React.FC = () => {
       SplashScreen.hideAsync();
     }, 1000);
     return () => clearInterval(interval);
-  }, [])
+  }, []);
 
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: '#121212', position: 'relative' }}>
-      <NotificationProvider webref={webViewRef} isLoading={isLoading} />
+    <SafeAreaView style={{ flex: 1, backgroundColor, position: 'relative' }}>
+      {/* <NotificationProvider webref={webViewRef} isLoading={isLoading} /> */}
       {isLoading && (
-        <View style={{
+        <View
+          style={{
             flex: 5,
             justifyContent: 'center',
             alignItems: 'center',
             width: '100%',
             height: '100%',
-        }}>
+          }}>
           <ActivityIndicator color="#f280a1" size="large" />
         </View>
       )}
       <WebView
         source={{ uri: WEBSITE_URL }}
         ref={webViewRef}
-        
+        forceDarkOn={colorScheme !== 'light'}
+        injectedJavaScript={INTIIAL_JAVASCRIPT_CODE}
+        onMessage={(event) => {
+          const msg = event.nativeEvent.data;
+          if (msg === 'light' || msg === 'dark') {
+            onColorChange(msg);
+          }
+        }}
         onLoadEnd={() => {
           SplashScreen.hideAsync();
           setIsLoading(false);
@@ -62,15 +106,41 @@ const MainView: React.FC = () => {
         }}
         renderError={() => {
           return (
-            <View style={{ flex: 10000, justifyContent: 'center', alignItems: 'center', paddingHorizontal: 16 }}>
-              <Text style={{ color: 'white', fontSize: 20, fontWeight: 'bold' }}>
+            <View
+              style={{
+                flex: 10000,
+                justifyContent: 'center',
+                alignItems: 'center',
+                paddingHorizontal: 16,
+              }}>
+              <Text
+                style={{
+                  color: colorScheme === 'light' ? 'black' : 'white',
+                  fontSize: 20,
+                  fontWeight: 'bold',
+                }}>
                 Unable to connect to
-                <Text style={{color: '#f280a1'}} onPress={() => Linking.openURL('https://dsek.se')}> D-sek </Text>
+                <Text
+                  style={{ color: '#f280a1' }}
+                  onPress={() => Linking.openURL('https://dsek.se')}>
+                  {' '}
+                  D-sek{' '}
+                </Text>
                 servers.
               </Text>
-              <Text style={{ color: 'white', fontSize: 16, fontWeight: '600' }}>
-                Try again later or contact 
-                <Text style={{color: '#f280a1'}} onPress={() => Linking.openURL('mailto:dwww@dsek.se')}> dwww@dsek.se</Text>
+              <Text
+                style={{
+                  color: colorScheme === 'light' ? 'black' : 'white',
+                  fontSize: 16,
+                  fontWeight: '600',
+                }}>
+                Try again later or contact
+                <Text
+                  style={{ color: '#f280a1' }}
+                  onPress={() => Linking.openURL('mailto:dwww@dsek.se')}>
+                  {' '}
+                  dwww@dsek.se
+                </Text>
               </Text>
             </View>
           );
@@ -82,17 +152,23 @@ const MainView: React.FC = () => {
         sharedCookiesEnabled
         onNavigationStateChange={(newNavState) => {
           if (
-            !newNavState.url.startsWith(WEBSITE_URL) && 
+            !newNavState.url.startsWith(WEBSITE_URL) &&
             !newNavState.url.startsWith('https://dsek.se') &&
             !newNavState.url.startsWith('https://www.dsek.se') &&
             !newNavState.url.includes('portal.dsek.se')
           ) {
             webViewRef.current.stopLoading();
             Linking.openURL(newNavState.url);
-          } else if (newNavState.url.startsWith('https://dsek.se') || newNavState.url.startsWith('https://www.dsek.se')) {
+          } else if (
+            newNavState.url.startsWith('https://dsek.se') ||
+            newNavState.url.startsWith('https://www.dsek.se')
+          ) {
             webViewRef.current.stopLoading();
             webViewRef.current.injectJavaScript(`
-              window.location = '${newNavState.url.replace('https://dsek.se', 'https://app.dsek.se')}';
+              window.location = '${newNavState.url.replace(
+                'https://dsek.se',
+                'https://app.dsek.se'
+              )}';
             `);
           }
         }}
