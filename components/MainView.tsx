1c0e7e608cea763e2865db5d4a0c4f46269c45c1
Ludvig <ludvig.svedberg@mac.com>
2023-04-19 00:06:49 +0200

    Fix refresh control and old dev constants

--- components/MainView.tsx
+++ components/MainView.tsx
@@ -5,18 +5,23 @@ import {
   BackHandler,
   Linking,
   Platform,
+  RefreshControl,
   SafeAreaView,
+  ScrollView,
   Text,
   View,
 } from 'react-native';
 import WebView from 'react-native-webview';
 
-const WEBSITE_URL = 'https://app.dsek.se';
+import NotificationProvider from '~/providers/NotificationProvider';
+
+const WEBSITE_URL = 'https://dsek.se';
 
 const DARK_COLOR = '#121212';
 const LIGHT_COLOR = '#fff';
 
-const INTIIAL_JAVASCRIPT_CODE = `(function() {
+const INTIIAL_JAVASCRIPT_CODE = `
+(function() {
   const mode = window.localStorage.getItem('mode');
   const updateMode = (newMode) => {
     if (newMode) {
@@ -36,7 +41,9 @@ const INTIIAL_JAVASCRIPT_CODE = `(function() {
     return result;
   };
   object[method] = newMethod.bind(object);
-})();`;
+})();
+
+true;`;
 
 const MainView: React.FC<{
   colorScheme: 'light' | 'dark';
@@ -72,7 +79,7 @@ const MainView: React.FC<{
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor, position: 'relative' }}>
-      {/* <NotificationProvider webref={webViewRef} isLoading={isLoading} /> */}
+      <NotificationProvider webref={webViewRef} isLoading={isLoading} />
       {isLoading && (
         <View
           style={{
@@ -85,94 +92,105 @@ const MainView: React.FC<{
           <ActivityIndicator color="#f280a1" size="large" />
         </View>
       )}
-      <WebView
-        source={{ uri: WEBSITE_URL }}
-        ref={webViewRef}
-        forceDarkOn={colorScheme !== 'light'}
-        injectedJavaScript={INTIIAL_JAVASCRIPT_CODE}
-        onMessage={(event) => {
-          const msg = event.nativeEvent.data;
-          if (msg === 'light' || msg === 'dark') {
-            onColorChange(msg);
-          }
+      <ScrollView
+        style={{
+          backgroundColor: 'transparent',
+          position: 'relative',
         }}
-        onLoadEnd={() => {
-          SplashScreen.hideAsync();
-          setIsLoading(false);
-        }}
-        onError={() => {
-          SplashScreen.hideAsync();
-          setIsLoading(false);
-        }}
-        renderError={() => {
-          return (
-            <View
-              style={{
-                flex: 10000,
-                justifyContent: 'center',
-                alignItems: 'center',
-                paddingHorizontal: 16,
-              }}>
-              <Text
+        contentContainerStyle={{ flex: 1 }}
+        refreshControl={
+          <RefreshControl refreshing={false} onRefresh={webViewRef?.current?.reload} />
+        }>
+        <WebView
+          source={{ uri: WEBSITE_URL }}
+          ref={webViewRef}
+          forceDarkOn={colorScheme !== 'light'}
+          injectedJavaScript={INTIIAL_JAVASCRIPT_CODE}
+          onMessage={(event) => {
+            const msg = event.nativeEvent.data;
+            if (msg === 'light' || msg === 'dark') {
+              onColorChange(msg);
+            }
+          }}
+          onLoadEnd={() => {
+            SplashScreen.hideAsync();
+            setIsLoading(false);
+          }}
+          onError={() => {
+            SplashScreen.hideAsync();
+            setIsLoading(false);
+          }}
+          renderError={() => {
+            return (
+              <View
                 style={{
-                  color: colorScheme === 'light' ? 'black' : 'white',
-                  fontSize: 20,
-                  fontWeight: 'bold',
+                  flex: 10000,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  paddingHorizontal: 16,
                 }}>
-                Unable to connect to
                 <Text
-                  style={{ color: '#f280a1' }}
-                  onPress={() => Linking.openURL('https://dsek.se')}>
-                  {' '}
-                  D-sek{' '}
+                  style={{
+                    color: colorScheme === 'light' ? 'black' : 'white',
+                    fontSize: 20,
+                    fontWeight: 'bold',
+                  }}>
+                  Unable to connect to
+                  <Text
+                    style={{ color: '#f280a1' }}
+                    onPress={() => Linking.openURL('https://dsek.se')}>
+                    {' '}
+                    D-sek{' '}
+                  </Text>
+                  servers.
                 </Text>
-                servers.
-              </Text>
-              <Text
-                style={{
-                  color: colorScheme === 'light' ? 'black' : 'white',
-                  fontSize: 16,
-                  fontWeight: '600',
-                }}>
-                Try again later or contact
                 <Text
-                  style={{ color: '#f280a1' }}
-                  onPress={() => Linking.openURL('mailto:dwww@dsek.se')}>
-                  {' '}
-                  dwww@dsek.se
+                  style={{
+                    color: colorScheme === 'light' ? 'black' : 'white',
+                    fontSize: 16,
+                    fontWeight: '600',
+                  }}>
+                  Try again later or contact
+                  <Text
+                    style={{ color: '#f280a1' }}
+                    onPress={() => Linking.openURL('mailto:dwww@dsek.se')}>
+                    {' '}
+                    dwww@dsek.se
+                  </Text>
                 </Text>
-              </Text>
-            </View>
-          );
-        }}
-        pullToRefreshEnabled
-        decelerationRate="normal"
-        style={{ flex: isLoading ? 0 : 1, backgroundColor: 'transparent' }}
-        allowsBackForwardNavigationGestures /* for swipe navigation on iOS */
-        sharedCookiesEnabled
-        onNavigationStateChange={(newNavState) => {
-          if (
-            !newNavState.url.startsWith(WEBSITE_URL) &&
-            !newNavState.url.startsWith('https://dsek.se') &&
-            !newNavState.url.startsWith('https://www.dsek.se') &&
-            !newNavState.url.includes('portal.dsek.se')
-          ) {
-            webViewRef.current.stopLoading();
-            Linking.openURL(newNavState.url);
-          } else if (
-            newNavState.url.startsWith('https://dsek.se') ||
-            newNavState.url.startsWith('https://www.dsek.se')
-          ) {
-            webViewRef.current.stopLoading();
-            webViewRef.current.injectJavaScript(`
-              window.location = '${newNavState.url.replace(
-                'https://dsek.se',
-                'https://app.dsek.se'
-              )}';
-            `);
-          }
-        }}
-      />
+              </View>
+            );
+          }}
+          pullToRefreshEnabled
+          setBuiltInZoomControls={false}
+          textZoom={0}
+          decelerationRate="normal"
+          style={{ flex: isLoading ? 0 : 1, backgroundColor: 'transparent' }}
+          allowsBackForwardNavigationGestures /* for swipe navigation on iOS */
+          sharedCookiesEnabled
+          onNavigationStateChange={(newNavState) => {
+            if (
+              !newNavState.url.startsWith(WEBSITE_URL) &&
+              !newNavState.url.startsWith('https://dsek.se') &&
+              !newNavState.url.startsWith('https://www.dsek.se') &&
+              !newNavState.url.includes('portal.dsek.se')
+            ) {
+              webViewRef.current.stopLoading();
+              Linking.openURL(newNavState.url);
+            } else if (
+              newNavState.url.startsWith('https://dsek.se') ||
+              newNavState.url.startsWith('https://www.dsek.se')
+            ) {
+              webViewRef.current.stopLoading();
+              webViewRef.current.injectJavaScript(`
+                window.location = '${newNavState.url
+                  .replace('https://dsek.se', 'https://app.dsek.se')
+                  .replace('https://www.dsek.se', 'https://app.dsek.se')}';
+              `);
+            }
+          }}
+        />
+      </ScrollView>
     </SafeAreaView>
   );
 };
