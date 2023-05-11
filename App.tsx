60221147a1c0114e8cab0dee71e77ad93ded0f4f
Ludvig <ludvig.svedberg@mac.com>
2023-05-11 17:25:12 +0200

    Fix top notch padding on android

--- App.tsx
+++ App.tsx
@@ -2,15 +2,17 @@ import * as NavigationBar from 'expo-navigation-bar';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect } from 'react';
-import { Platform, useColorScheme } from 'react-native';
+import { Platform, useColorScheme, StatusBar as RNStatusBar } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import MainView from '~/components/MainView';
+import { COLORS } from '~/globals';
 
 SplashScreen.preventAutoHideAsync();
 const App = () => {
   const preferredColorScheme = useColorScheme();
   const [colorScheme, setColorScheme] = React.useState<'light' | 'dark'>(preferredColorScheme);
+  const colors = colorScheme === 'light' ? COLORS.light : COLORS.dark;
 
   useEffect(() => {
     if (Platform.OS === 'android') {
@@ -20,7 +22,11 @@ const App = () => {
   }, [colorScheme]);
 
   return (
-    <SafeAreaProvider>
+    <SafeAreaProvider
+      style={{
+        backgroundColor: colors.headers,
+        paddingTop: Platform.OS === 'android' ? RNStatusBar.currentHeight : 0,
+      }}>
       <MainView colorScheme={colorScheme} onColorChange={setColorScheme} />
       <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
     </SafeAreaProvider>
